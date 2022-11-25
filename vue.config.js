d72ad1b714d71b6b68e3e7f25ada463ffa5d9c87
peng ly <ly.peng@gempoll.com>
2022-11-25 19:17:45 +0800

    feat(main): alias

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require("@vue/cli-service")
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // outputDir: "./build",
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: "@/component"
+  //     }
+  //   }
+  // }
+  // configureWebpack: config => {
+  //   config.resolve.alias = {
+  //     "@": Path2D.resolve(__dirname, "src")
+  //   }
+  // }
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', path.resolve('src'))
+      .set('components', '@/components')
+  }
 })
